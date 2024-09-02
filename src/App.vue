a6e3955be916a255f6fa52b3fd1b69c953aafd65
GEVIC21 <ndilbedouvic@gmail.com>
2024-09-02 22:07:16 +0000

    Premier commit vue ts

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import type { ItemInterface } from './models/items/Item.interface'
+import ItemsListComponent from './components/items/ItemsList.component.vue'
+
+  // mock some data:
+  const items: ItemInterface[] = [{
+    id: 1,
+    name: 'Item 1',
+    selected:false
+  }, {
+    id: 2,
+    name: 'Item 2',
+    selected:false
+  }, {
+    id: 3,
+    name: 'Item 3',
+    selected:false
+  }]
+</script>
+
+
+<template>
+  <main>
+    <div class="home">
+      <ItemsListComponent :items="items" />
+    </div>
+  </main>
+</template>
---
+++ src/components/items/ItemsList.component.vue
@@ -0,0 +1,19 @@
+import { ItemInterface } from '../../models/items/Item.interface';
+<script setup lang="ts">
+import type { ItemInterface } from '../../models/items/Item.interface'
+    defineProps < {
+        items: ItemInterface[]
+    } > ()
+</script>
+
+<template>
+    <div>
+        <h3>Items:</h3>
+        <ul>
+            <li v-for="(item, index) in items" :key="item.id">
+                {{item.name}}
+            </li>
+        </ul>
+    </div>
+
+</template>
